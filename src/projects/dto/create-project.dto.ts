32cebce7c60bdba4ff0053062fd8fdb6f7af5217
Florian <florianpaulschoene@gmail.com>
2025-08-29 14:48:57 +0200

    remove userId from CreateProjectDto, userId will evaluated in the endpoint

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -3,10 +3,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateProjectDto {
   @IsNotEmpty()
   @IsString()
-  userId: string;
-
-  @IsNotEmpty()
-  @IsString()
   name: string;
 
   @IsString()
