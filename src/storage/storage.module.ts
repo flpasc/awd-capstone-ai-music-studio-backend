72e245db6ca31fae423fa5fbf7ee6afc8ecfd7c7
Wizzel1 <43343510+Wizzel1@users.noreply.github.com>
2025-09-15 14:43:20 +0200

    feature/generate audio by prompt (#24)
    * Install fal-client dependency
    
    * Add env to example
    
    * Add ai resource
    
    * Install dependency
    
    * Fix parsing
    
    * Throw error when audio file cant be fetched
    
    * Split prompt input

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { StorageService } from './storage.service';
-import { StorageController } from './storage.controller';
 import { AuthService } from 'src/auth/auth.service';
+import { StorageController } from './storage.controller';
+import { StorageService } from './storage.service';
 
 @Module({
   controllers: [StorageController],
   providers: [StorageService, AuthService],
+  exports: [StorageService],
 })
 export class MinioModule {}
